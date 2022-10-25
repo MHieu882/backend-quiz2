81d8408a3b6ef82d213f35f1f232bd046df2d62c
Lê Minh Hiếu <84466517+MHieu882@users.noreply.github.com>
2022-10-25 09:39:52 +0700

    Update ExTime.js

--- ExTime.js
+++ ExTime.js
@@ -13,12 +13,12 @@ function findtoStartEndofMoth(date,timezone="Africa/Freetown"){
         endOfMonth
     }
 }
-console.log(findtoStartEndofMoth(new Date('October 1, 2022'),'Antarctica/Mawson'));
+console.log(findtoStartEndofMoth(new Date('October 1, 2022'),'Asia/Ho_Chi_Minh'));
 
 function mtdperiod(date,timezone="Africa/Freetown"){
-    if(date.getDate()==1){
-        date=moment(date).add(1,'day')
-    }
+    // if(date.getDate()==1 && timezone!='Asia/...'){
+    //     date=moment(date).add(1,'day')
+    // }
     const startOfMonth= moment(date).tz(timezone).startOf('month').format();
     const presentday= moment(date).tz(timezone).endOf('day').format();
     return {
@@ -26,7 +26,7 @@ function mtdperiod(date,timezone="Africa/Freetown"){
         presentday
     }
 }
-console.log(mtdperiod(new Date('October 1, 2022'),'Antarctica/Mawson'));
+console.log(mtdperiod(new Date('October 1, 2022'),'Asia/Ho_Chi_Minh'));
 
 function MonSun(date,timezone="Africa/Freetown"){
     const newdate= date.setDate(date.getDate()-7);
